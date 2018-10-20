c0ae1164cc39475739a899dd4b2eb468870fed6a
Jhonbeltran <jbeltranleon@gmail.com>
2018-10-19 22:34:58 -0500

    Agrego recepción y evidencia de objeto mensaje

--- app.js
+++ app.js
@@ -16,13 +16,24 @@ app.get('/', function (req, response) {
     response.send('Hola Mundo!')
 })
 
-/* Verificación de token */
+/* Verificación de token esta url la pondremos en /messenger/settings/*/
 app.get('/webhook', function (req, response){
     if (req.query['hub.verify_token'] === 'polly_token'){
         response.send(req.query['hub.challenge']);
     } else {
         response.send('No tienes Permisos suficientes, lo siento');
     }
+})
+
+/* Validar que exista un objeto mensaje y retornarlo */
+app.post('/webhook', function (req, res) {
+    const webhook_event = req.body.entry[0];
+    if(webhook_event.messaging){
+        webhook_event.messaging.forEach(event => {
+            console.log(event);
+        })
+    }
+    res.sendStatus(200);
 })
 
 /* Log de Funcionaminto */
