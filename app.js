38674ceeea2497a489d6db503982d323f4438d34
Jhonbeltran <jbeltranleon@gmail.com>
2018-10-19 19:52:27 -0500

    Agrego verificacion de token y listener de funcionamiento

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const request = requiere('request');
+const request = require('request');
 
 const app = express();
 
@@ -11,4 +11,16 @@ app.use(bodyParser.json());
 
 app.get('/', function (request, response) {
     response.send('Hola Mundo!')
+})
+
+app.get('/webhook', function (request, response){
+    if (request.query['hub.verify_token'] === 'polly_token'){
+        response.send(request.query['hub.challenge']);
+    } else {
+        response.send('No tienes Permisos suficientes, lo siento');
+    }
+})
+
+app.listen(app.get('port'), function(){
+    console.log('Servidor funcionando bien aspero en el puerto', app.get('port'));
 })
