6e94510aa93f6752a327251897f69632be817eca
Jhonbeltran <jbeltranleon@gmail.com>
2018-11-03 22:01:51 -0500

    Agrego log para verificar nlp

--- app.js
+++ app.js
@@ -59,6 +59,7 @@ function handleMessage(senderId, event) {
     const greeting = firstEntity(event.nlp, 'greetings');
     if (greeting && greeting.confidence > 0.8) {
         console.log(greeting)
+        console.log("Saludando ando")
     } 
     if (event.text == 'Busco a Fredy') {
         contactSupport(senderId);
